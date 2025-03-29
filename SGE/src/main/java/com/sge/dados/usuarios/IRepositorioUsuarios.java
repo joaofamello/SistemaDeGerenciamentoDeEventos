bbbd881873d2c92a8832d5895d0eee20f4df30e8
João Francisco <joaofrancisco.amello@gmail.com>
2025-03-29 11:44:47 -0300

    Criei os repositórios

---
+++ SGE/src/main/java/com/sge/dados/usuarios/IRepositorioUsuarios.java
@@ -0,0 +1,4 @@
+package com.sge.dados.usuarios;
+
+public interface IRepositorioUsuarios {
+}
---
+++ SGE/src/main/java/com/sge/dados/usuarios/RepositorioUsuariosArrayList.java
@@ -0,0 +1,4 @@
+package com.sge.dados.usuarios;
+
+public class RepositorioUsuariosArrayList implements IRepositorioUsuarios {
+}
