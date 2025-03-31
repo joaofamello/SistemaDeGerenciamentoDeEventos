e07c65f012480f0192d608e4ebabd07dce5edcd8
Guilherme Henrique <guihbslima200@gmail.com>
2025-03-31 20:35:08 -0300

    Gerenciamento de Repositorios(2º)
    Atualização nas classes de gerenciamento de repositorio

--- SGE/src/main/java/com/sge/negocio/NegocioEvento.java
+++ SGE/src/main/java/com/sge/negocio/NegocioEvento.java
@@ -4,8 +4,23 @@ import com.sge.dados.eventos.IRepositorioEventos;
 import com.sge.dados.eventos.RepositorioEventosArrayList;
 import com.sge.negocio.entidade.Evento;
 
-
-
 public class NegocioEvento {
+    private IRepositorioEventos repositorioEventos;
+
+    public NegocioEvento(IRepositorioEventos repositorioEventos) {
+        this.repositorioEventos = repositorioEventos;
+    }
+
+    public void inserir(Evento evento) {
+        repositorioEventos.inserir(evento);
+    }
+    public void alterar(Evento evento) {
+        repositorioEventos.alterar(evento);
+    }
+    public void remover(Evento evento) {
+        repositorioEventos.remover(evento);
+    }
+
+
 
 }
