888ab87fafff8d3ec4f91edad4cd0da68bf65b8d
Guilherme Henrique <guihbslima200@gmail.com>
2025-03-31 20:09:44 -0300

    Gerenciar Repositorios
    Classes para gerenciar os repositorios

--- SGE/src/main/java/com/sge/negocio/NegocioEvento.java
+++ SGE/src/main/java/com/sge/negocio/NegocioEvento.java
@@ -1,4 +1,11 @@
 package com.sge.negocio;
 
+import com.sge.dados.eventos.IRepositorioEventos;
+import com.sge.dados.eventos.RepositorioEventosArrayList;
+import com.sge.negocio.entidade.Evento;
+
+
+
 public class NegocioEvento {
+
 }
--- SGE/src/main/java/com/sge/negocio/NegocioUsuario.java
+++ SGE/src/main/java/com/sge/negocio/NegocioUsuario.java
@@ -1,4 +1,8 @@
 package com.sge.negocio;
 
+import com.sge.dados.eventos.IRepositorioEventos;
+import com.sge.dados.eventos.RepositorioEventosArrayList;
+import com.sge.negocio.entidade.Evento;
+
 public class NegocioUsuario {
 }
