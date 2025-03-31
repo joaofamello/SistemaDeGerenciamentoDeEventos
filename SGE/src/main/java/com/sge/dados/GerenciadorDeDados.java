665c8c4ade54b28136845deac15bb2ccca5dda56
Jurandirtvaz <jurandirtvaz28@gmail.com>
2025-03-31 20:36:20 -0300

    Criação do Gerenciador de Arquivos
    Essa classe está responsável por inicializar o arquivo caso o usuário não tenha a pasta do "Banco de Dados". Ou seja, ele cria os arquivos TXT e os armazena dentro de um pacote.

--- SGE/src/main/java/com/sge/dados/GerenciadorDeDados.java
+++ SGE/src/main/java/com/sge/dados/GerenciadorDeDados.java
@@ -4,13 +4,25 @@ import java.io.*;
 import java.nio.file.*;
 
 public class GerenciadorDeDados {
-    private static final Path Users_Path = Paths.get(System.getProperty("user.dir"), "UsersData.txt");
-    private static final Path Events_Path = Paths.get(System.getProperty("user.dir"), "EventsData.txt");
+    private static final Path Diretorio = Paths.get(System.getProperty("user.dir"), "SGE/BancoDeDados");
+    private static final Path Pasta_Usuarios = Diretorio.resolve("UsersData.txt");
+    private static final Path Pasta_Eventos = Diretorio.resolve("EventsData.txt");
 
     //Bloco para inicializar estaticamente os arquivos
     static {
-        criarArquivo(Users_Path);
-        criarArquivo(Events_Path);
+        criarDiretorio(Diretorio);
+        criarArquivo(Pasta_Usuarios);
+        criarArquivo(Pasta_Eventos);
+    }
+
+    private static void criarDiretorio(Path diretorio) {
+        try{
+            if(!Files.exists(diretorio)) {
+                Files.createDirectories(diretorio);
+            }
+        } catch(IOException e){
+            System.err.println("Erro ao criar diretório: " + e.getMessage());
+        }
     }
 
     private static void criarArquivo(Path caminho) {
