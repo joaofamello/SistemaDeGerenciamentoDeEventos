02e7533be1f4d3f3f67e38a226b9219c11718027
Guilherme Henrique <guihbslima200@gmail.com>
2025-03-31 18:51:57 -0300

    Repositório Usuarios
    Base do repositório usuarios

--- SGE/src/main/java/com/sge/dados/usuarios/RepositorioUsuariosArrayList.java
+++ SGE/src/main/java/com/sge/dados/usuarios/RepositorioUsuariosArrayList.java
@@ -1,4 +1,34 @@
 package com.sge.dados.usuarios;
 
+import com.sge.negocio.entidade.Usuario;
+
+import java.util.ArrayList;
+
 public class RepositorioUsuariosArrayList implements IRepositorioUsuarios {
+
+    private ArrayList<Usuario> usuarios;
+
+    private RepositorioUsuariosArrayList() { usuarios = new ArrayList<Usuario>();}
+
+    @Override
+    public void inserir(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    @Override
+    public Usuario buscar(int id) {
+        Usuario usuario = null;
+        for (Usuario usuario1 : usuarios) {
+            if(usuario.getID() == id) {
+                usuario = usuario1;
+                break;
+            }
+        }
+        return usuario;
+    }
+
+    @Override
+    public ArrayList<Usuario> listar() {
+        return null;
+    }
 }
