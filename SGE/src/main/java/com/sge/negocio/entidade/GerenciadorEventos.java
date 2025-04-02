d96812010f70b92253c34a9b3fad3968dd1c6c76
João Francisco <joaofrancisco.amello@gmail.com>
2025-04-02 20:28:52 -0300

    Corrigi erros do merge

--- SGE/src/main/java/com/sge/negocio/entidade/GerenciadorEventos.java
+++ SGE/src/main/java/com/sge/negocio/entidade/GerenciadorEventos.java
@@ -12,68 +12,100 @@ public class GerenciadorEventos {
     }
 
     //Métodos Básicos de Gerenciamento dos Eventos
-    public String listarEventos() {
-        if(operador.getEventosCriados() != null){
-            for(Evento evento : operador.getEventosCriados()){
-                return evento.eventoFormatado();
+    public void listarEventos() {
+        if (operador.getEventosCriados() != null) {
+            for (Evento evento : operador.getEventosCriados()) {
+                System.out.println("\nTitulo: " + evento.getTitulo()
+                        + "\nDescrição: " + evento.getDescricao()
+                        + "\nCategoria: " + evento.getCategoria()
+                        + "\nEndereço:  "
+                        + "\nEstado: " + evento.getEndereco().getEstado()
+                        + "\nCidade: " + evento.getEndereco().getCidade()
+                        + "\nCEP: " + evento.getEndereco().getCep()
+                        + "\nBairro: " + evento.getEndereco().getBairro()
+                        + "\nRua: " + evento.getEndereco().getRua()
+                        + "\nNumero: " + evento.getEndereco().getNumero()
+                        + "\nTitulo: " + evento.getTitulo());
             }
+        } else {
+            System.out.println("Nenhum evento cadastrado!"); // Criar um exception para isso
         }
-        return null;
     }
 
-    public String buscarMeuEventoNome() {
+    public Evento buscarEventoNome() {
         String nome = entrada.recebeString();
         for (Evento evento : operador.getEventosCriados()) {
             if(nome.equals(evento.getTitulo())) {
-                return evento.eventoFormatado();
+                return evento;
             }
         }
         return null; // usar o exception de nenhum evento cadastrado
     }
-
-    public String buscarMeuEventoCategoria() {
-        String categoria = entrada.recebeString();
+    public Evento buscarEventoID() {
+        int ID = entrada.recebeInt();
         for (Evento evento : operador.getEventosCriados()) {
-            if(categoria.equals(evento.getCategoria())) {
-                return evento.eventoFormatado();
+            if(ID == evento.getID()){
+                return evento;
             }
         }
         return null;
     }
 
-    public String mudarTituloMeuEvento(Evento evento, String novoTitulo) {
+    public void mudarTituloEvento(Evento evento, String novoTitulo) {
         evento.setTitulo(novoTitulo);
-        System.out.println("Evento editado!");
-        return evento.eventoFormatado();
     }
 
-    public void mudarDescricaoMeuEvento(Evento evento, String novaDescricao) {
+    public void mudarDescricaoEvento(Evento evento, String novaDescricao) {
         evento.setDescricao(novaDescricao);
     }
 
-    public void mudarDataMeuEvento(Evento evento, LocalDate novaData){
+    public void mudarDataEvento(Evento evento, LocalDate novaData){
         evento.setData(novaData);
     }
 
-    public void mudarHoraMeuEvento(Evento evento, LocalDateTime novoInicio, LocalDateTime novoFim) {
+    public void mudarHoraEvento(Evento evento, LocalDateTime novoInicio, LocalDateTime novoFim) {
         evento.setHoraInicio(novoInicio);
         evento.setHoraFim(novoFim);
     }
 
-    public void mudarEnderecoMeuEvento(Evento evento, Endereco endereco) {
+    public void mudarEnderecoEvento(Evento evento, Endereco endereco) {
         evento.setEndereco(endereco);
     }
 
-    public void mudarCategoriaMeuEvento(Evento evento, String categoria) {
+    public void mudarCategoriaEvento(Evento evento, String categoria) {
         evento.setCategoria(categoria);
     }
 
-    public void mudarQtdeIngressosMeuEvento(Evento evento, int qtdeIngressos) {
+    public void mudarQtdeIngressosEvento(Evento evento, int qtdeIngressos) {
         evento.setQtdeIngressos(qtdeIngressos);
     }
 
-    public void excluirMeuEvento(Evento evento) { // pensar melhor nisso aqui
+    public void excluirEvento(Evento evento) { // pensar melhor nisso aqui
         operador.getEventosCriados().remove(evento);
     }
 
+    public void editarEvento(Evento evento) { // nisso aqui também
+        // criar exceção para evento não encontrado
+        // criar uma tela para edição
+
+        switch(entrada.recebeInt()){
+            case 1:
+                evento.setTitulo(entrada.recebeString());
+                break;
+            case 2:
+                evento.setDescricao(entrada.recebeString());
+                break;
+            case 3:
+                evento.setCategoria(entrada.recebeString());
+                break;
+            case 4:
+                Endereco novoEndereco = entrada.criarEndereco();
+                evento.setEndereco(novoEndereco);
+                break;
+            case 5:
+                evento.setQtdeIngressos(entrada.recebeInt());
+                break;
+            // ainda tem coisas para fazer
+        }
+    }
 }
