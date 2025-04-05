0914b95efe4ea41d3dde4e81df2227ec51bd5509
Jurandirtvaz <jurandirtvaz28@gmail.com>
2025-04-05 17:53:51 -0300

    Criação da Classe Abstrata Ingresso
    Criação da classe abstrata ingresso e suas dependentes.

---
+++ SGE/src/main/java/com/sge/negocio/entidade/ingresso/IngressoInteiro.java
@@ -0,0 +1,15 @@
+package com.sge.negocio.entidade.ingresso;
+
+import com.sge.negocio.entidade.Evento;
+
+public class IngressoInteiro extends Ingresso {
+
+    public IngressoInteiro(Evento evento, double valorBase){
+        super(evento, valorBase);
+    }
+
+    @Override
+    public double calcularValorFinal(){
+        return getValorBase(); //Retorna o valor sem desconto
+    }
+}
